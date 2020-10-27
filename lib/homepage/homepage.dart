d3610d088cb5fabe47f0d67b260882b9590d53f6
jithin <iamjithinjohnson@gmail.com>
2020-10-27 17:05:42 +0530

    button added

--- lib/homepage/homepage.dart
+++ lib/homepage/homepage.dart
@@ -8,7 +8,7 @@ class HomePage extends StatelessWidget {
   final List<Widget> bodycontent = [
     Text('history'),
     Text('apps'),
-    Text('home'),
+    Share(),
     Text('bio'),
     Text('info'),
   ];
@@ -68,4 +68,57 @@ class NavController extends GetxController {
   final _selectedIndex = 0.obs;
   get selectedIndex => this._selectedIndex.value;
   set selectedIndex(index) => this._selectedIndex.value = index;
+}
+
+class Share extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          'Send',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 10),
+          child: CustomButton(),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 10),
+          child: Text(
+            'Recieve',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Center(
+          child: Container(
+            height: 257,
+            width: 72,
+            decoration: BoxDecoration(
+                color: Color(0xFF6D6969),
+                borderRadius: BorderRadius.all(Radius.circular(70))),
+          ),
+        ),
+        Center(
+          child: Container(
+            height: 95,
+            width: 95,
+            decoration:
+                BoxDecoration(color: Color(0xFF7F6BF9), shape: BoxShape.circle),
+          ),
+        )
+      ],
+    );
+  }
 }
