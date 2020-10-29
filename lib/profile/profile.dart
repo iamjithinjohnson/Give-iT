f7b861a05a354f5e8bc136c50e0ac5fe440975be
jithin <iamjithinjohnson@gmail.com>
2020-10-29 14:32:33 +0530

    getx added in profile

--- lib/profile/profile.dart
+++ lib/profile/profile.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class Profile extends StatelessWidget {
+  //final Pushname pushname = Get.put(Pushname());
+
+  final String name = "Give-iT";
+  static var myController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -10,41 +16,131 @@ class Profile extends StatelessWidget {
           padding: const EdgeInsets.only(top: 20),
           child: CircleAvatar(
             // ====== Image Profile ======
-            child: Text(
-              'G',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-                fontSize: 50,
-              ),
+            child: GetBuilder<Pushname>(
+              init: Pushname(),
+              builder: (_) {
+                if (_.namee == null || _.namee == '') {
+                  _.namee = "Give-iT";
+                }
+                return Text(
+                  "${_.namee[0]}",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontSize: 50,
+                  ),
+                );
+              },
             ),
+            // Text(
+            //   'G',
+            //   style: TextStyle(
+            //     fontWeight: FontWeight.bold,
+            //     color: Colors.white,
+            //     fontSize: 50,
+            //   ),
+            // ),
             radius: size.width / 6,
             backgroundColor: Color(0xFF009533),
           ),
         ),
         Padding(
-            padding: const EdgeInsets.only(top: 8.0),
-            child: RichText(
-                text: TextSpan(children: [
-              TextSpan(
-                text: 'Give-iT',
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    color: Colors.black),
+          padding: const EdgeInsets.only(left: 40),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              GetBuilder<Pushname>(
+                  init: Pushname(),
+                  builder: (_) {
+                    if (_.namee == null || _.namee == '') {
+                      _.namee = "Give-iT";
+                    }
+
+                    return Text(
+                      "${_.namee}",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.black,
+                      ),
+                    );
+                  }),
+              //======= Profile name changer ========
+              IconButton(
+                icon: Icon(Icons.create),
+                //======= show dialog for text change ======
+                onPressed: () {
+                  return showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        shape: RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(20))),
+                        actions: [
+                          FlatButton(
+                            onPressed: () => Navigator.pop(context),
+                            child: Text(
+                              'Cancel',
+                              style: TextStyle(color: Colors.blue),
+                            ),
+                          ),
+                          FlatButton(
+                            onPressed: () async {
+                              //Pushname(namee: myController.text);
+                              await Get.find<Pushname>().nameof();
+                              Navigator.pop(context);
+                            },
+                            child: Text(
+                              'Ok',
+                              style: TextStyle(color: Colors.blue),
+                            ),
+                          ),
+                        ],
+                        content: TextField(
+                          controller: myController,
+                          textAlign: TextAlign.center,
+                          autofocus: true,
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: ' enter name',
+                            hintStyle: new TextStyle(
+                              color: Colors.grey.shade500,
+                              fontSize: 12.0,
+                              fontFamily: 'helvetica_neue_light',
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                },
               ),
-              WidgetSpan(
-                  child: Icon(
-                Icons.create,
-                size: 20,
-              ))
-            ]))
-            //     Text(
-            //   'Give-iT',
-            //   style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-            // ),
-            )
+            ],
+          ),
+        )
       ],
     );
   }
+}
+
+class Pushname extends GetxController {
+  Pushname({this.namee});
+  String namee;
+  Future nameof() async {
+    namee = Profile.myController.text;
+    update();
+  }
+}
+
+//====== OBX GETX Study Purpose =======
+class ProfileName {
+  String name;
+  ProfileName({this.name = 'Give-iT'});
+}
+
+class ProfileController extends GetxController {
+  final profilename = ProfileName().obs;
+
+  //updateProfile();
 }
